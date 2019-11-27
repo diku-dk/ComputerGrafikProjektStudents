76d728ae02b56d9246402999c878a6a86dd08d21
Kenny Erleben <kenny@di.ku.dk>
2019-11-27 09:30:29 +0100

    Removed auto-generated files from repository
    These files are platform specific and should be generated on the local machine when running CMake.

--- GraphicsProject/Assignment-6/include/data_path.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATA_PATH_H
-#define DATA_PATH_H
-
-std::string const data_path = "/Users/kaiip/Documents/Teaching/FoundationsOfGraphics/2020/StudentProjects/GraphicsProject/Assignment-6/src/";
-
-//DATA_PATH_H
-#endif
