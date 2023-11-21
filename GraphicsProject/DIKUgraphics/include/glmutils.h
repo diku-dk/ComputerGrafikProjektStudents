c61b9273be04d0a388794c83d67ab9bc67087733
kaiip <kaiip@diku.dk>
2023-11-21 10:25:47 +0100

    Included <glm/gtx/norm.hpp> into glmutils.h

--- GraphicsProject/DIKUgraphics/include/glmutils.h
+++ GraphicsProject/DIKUgraphics/include/glmutils.h
@@ -21,8 +21,8 @@
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/integer.hpp>
+#include <glm/glx/norm.hpp>
 #include <glm/gtc/matrix_access.hpp>
-
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/gtx/transform2.hpp>
